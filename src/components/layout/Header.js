416b69bc143114e477a60b6973b920ea9989a58b
flash-of-genius <amhdaar@gmail.com>
2022-09-15 06:45:23 +0100

    src\components\layout\Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,1 +1,17 @@
-export const Header = () => <p>I am a header!</p>;
+export const Header = () => {
+  return (
+    <header className="header" data-testid="header">
+      <nav>
+        <div className="logo">
+          <img src="/images/logo.png" alt="Todoist" />
+        </div>
+        <div className="setting">
+          <ul>
+            <li>+</li>
+            <li>Pizza Slice</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
