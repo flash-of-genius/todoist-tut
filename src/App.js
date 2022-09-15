8d9434bcada6a3adea7cbc7e190796d05b7188ca
flash-of-genius <amhdaar@gmail.com>
2022-09-15 07:43:14 +0100

    src\App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Header } from "./components/layout/Header";
+import { Content } from "./components/layout/Content";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Content />
     </div>
   );
 };
