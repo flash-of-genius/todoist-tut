17bce1f17593ab9d522328aa8dd0848645fa90dc
flash-of-genius <amhdaar@gmail.com>
2022-09-15 08:20:35 +0100

    src\components\layout\Sidebar.js

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,1 +1,38 @@
-export const Sidebar = () => <p>I am a sidebar!!</p>;
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from "react-icons/fa";
+export const Sidebar = () => (
+  <div className="sidebar" data-testid="sidebar">
+    <ul className="sidebar__generic">
+      <li>
+        <span>
+          <FaInbox />
+        </span>
+        <span>Inbox</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendar />
+        </span>
+        <span>Today</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendarAlt />
+        </span>
+        <span>Next 7 days</span>
+      </li>
+    </ul>
+    <div className="sidebar__middle">
+      <span>
+        <FaChevronDown />
+        <h2>Projects</h2>
+      </span>
+    </div>
+    <ul className="sidebar__projects">Projects will be here!!</ul>
+    Add Project Component here !!!
+  </div>
+);
